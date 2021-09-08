7b7c475db4e8cf03d3204af168fcf91cc7de2818
ali khalse <wwwwwq37@gmail.com>
2021-09-08 13:56:14 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+#======================== MR_KHALSE | PHP KHALSE ==========================#
+#                                                                          #
+#           لطفا با معرفی ما و زدن منبع حمایتمون کنید.                 #
+#                   (@Sudo_khalse) =>  MR_KHALSE                           #
+#                   (@PHP_KHALSE) =>  PHP KHALSE                           #
+#                                                                          #
+#======================== MR_KHALSE | PHP KHALSE ==========================#
+
+$DATA = json_decode(file_get_contents("https://www.binance.com/api/v1/ticker/allPrices"),true);
+header("Content-type: application/json; charset=utf-8");
+function isValidCurrency($currency){
+global $DATA;
+$LISR = array_column($DATA, 'symbol');
+foreach($LISR as $key){
+$NAMEs[] = substr($key,0,3);
+}
+$currencyList = array_unique($NAMEs);
+return in_array($currency,$currencyList);
+}
+if(!empty($_REQUEST['n'])){
+$ARZ_NAME =  explode(",",strtoupper($_REQUEST['n']));
+$DOLAR =  $DATA[array_search("BTCUSDT", array_column($DATA, 'symbol'))]['price'];
+foreach($ARZ_NAME as $key => $value){
+if(isValidCurrency($value)){
+if($value == "BTC"){
+$PRICR = $DOLAR * 1;    
+}else{
+$key = array_search($value."BTC", array_column($DATA, 'symbol'));
+$PRICR = $DATA[$key]['price'] * $DOLAR;        
+}
+
+$RESULT[$value] = $PRICR ;
+}else{
+$RESULT[$value] = "Isn`t Vali Currency" ;
+}
+}
+}else{
+$RESULT['ERROR'] = "'n' is empty." ;   
+}
+echo json_encode(["MR_KHALSE"=>'PHP_KHALSE',"result"=>$RESULT]);
+
+#======================== MR_KHALSE | PHP KHALSE ==========================#
+#                                                                          #
+#           لطفا با معرفی ما و زدن منبع حمایتمون کنید.                 #
+#                   (@Sudo_khalse) =>  MR_KHALSE                           #
+#                   (@PHP_KHALSE) =>  PHP KHALSE                           #
+#                                                                          #
+#======================== MR_KHALSE | PHP KHALSE ==========================#
+?>
